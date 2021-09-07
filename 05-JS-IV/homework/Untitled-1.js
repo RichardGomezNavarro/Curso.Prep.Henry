83e5ca0c566bb2ab10d59c6310f6423750254547
unknown <Richard Gomez>
2021-09-07 12:34:45 -0500

    JS-VI homework done!

---
+++ 05-JS-IV/homework/Untitled-1.js
@@ -0,0 +1,22 @@
+var alumno = {
+    nombre: 'Juan',
+    edad: 15,
+    saludo:function () {
+        console.log("Hola,"+this.nombre)
+    }
+
+
+
+
+
+
+}
+console.log(alumno[nombre]())
+console.log(alumno.nombre="Ricard")
+
+console.log(alumno["nombre"])
+console.log((alumno["edad"]))
+console.log(alumno.saludo())
+console.log(delete alumno.edad)
+console.log(alumno)
+
